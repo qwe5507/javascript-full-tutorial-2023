9430ba5322516b8692ec9092351829e48ae03056
qwe5507 <apvmffkdls@gmail.com>
2024-08-06 00:48:48 +0900

    주석추가

--- 3_all_about_object/4_using_function_to_create_objects.js
+++ 3_all_about_object/4_using_function_to_create_objects.js
@@ -2,7 +2,7 @@
  * Using function to create objects
  */
 function IdolModel(name, year){
-    if(!new.target){
+    if(!new.target){ // new를 안붙이면 undefined
         return new IdolModel(name, year);
     }
 
@@ -18,12 +18,13 @@ const yuJin = new IdolModel('안유진', 2003);
 console.log(yuJin);
 // console.log(yuJin.dance());
 const yuJin2 = IdolModel('안유진', 2003);
-console.log(yuJin2);
-// console.log(global.name);
-
+console.log(yuJin2); // new를 안붙이면 함수에서 반환해주는게 없기때문에 undefined가 리턴된다.
+// console.log(global.name); // new를 안붙이면 this키워드가 global에 붙는다.
+// new를 쓰면 this가 인스턴스에 매핑되는데, new를 안쓰면 this가 global이 된다.
 const IdolModelArrow = (name, year)=>{
     this.name = name;
     this.year = year;
 };
 
-const yuJin3 = new IdolModelArrow('안유진', 2003);
+const yuJin3 = new IdolModelArrow('안유진', 2003); // 오류 : IdolModelArrow is not a constructor
+// 애로우 함수는 생성자 함수로 사용할 수없다.
