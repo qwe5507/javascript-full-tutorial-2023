0b1e1f5d908e0d904fa71f6b7adbb884c8857caa
qwe5507 <apvmffkdls@gmail.com>
2024-08-09 21:27:17 +0900

    주석추가

--- 3_all_about_object/6_scope.js
+++ 3_all_about_object/6_scope.js
@@ -6,18 +6,18 @@ var numberOne = 20;
 function levelOne(){
     console.log(numberOne);
 }
-
+// 상위스코프 접근가능
 // levelOne();
 
 function levelOne(){
     var numberOne = 40;
 
-    console.log(numberOne);
+    console.log(numberOne); // 40
 }
 
 // levelOne();
 
-console.log(numberOne);
+console.log(numberOne); // 20
 
 function levelOne(){
     var numberOne = 40;
@@ -25,17 +25,17 @@ function levelOne(){
     function levelTwo(){
         var numberTwo = 99;
 
-        console.log(`levelTwo numberTwo : ${numberTwo}`);
-        console.log(`levelTwo numberOne : ${numberOne}`);
+        console.log(`levelTwo numberTwo : ${numberTwo}`); // 99
+        console.log(`levelTwo numberOne : ${numberOne}`); // 40
     }
 
     levelTwo();
-    console.log(`levelOne numberOne : ${numberOne}`);
+    console.log(`levelOne numberOne : ${numberOne}`); // 40
 }
 
 levelOne();
-console.log(numberOne);
-// console.log(numberTwo);
+console.log(numberOne); // 20
+// console.log(numberTwo); // error
 
 /**
  * JS -> Lexical Scope
@@ -55,7 +55,7 @@ function functionOne(){
 }
 
 function functionTwo(){
-    console.log(numberThree);
+    console.log(numberThree); // 3 , JS는 렉시컬스코프기 때문에
 }
 
 functionOne();
@@ -65,14 +65,14 @@ var i = 999;
 for(var i = 0; i < 10; i++){
     console.log(i);
 }
-console.log(`i in global scope : ${i}`)
+console.log(`i in global scope : ${i}`) // 10
 
 i = 999;
 // block level scope
 for(let i = 0; i < 10; i++){
     console.log(i);
 }
-console.log(`i in global scope : ${i}`);
+console.log(`i in global scope : ${i}`); // 999
 
 /**
  * var 키워드는 함수 레벨 스코프만 만들어낸다.
