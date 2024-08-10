6a99ddc7f0925e395d2455c2292e036a845a8e39
qwe5507 <apvmffkdls@gmail.com>
2024-08-10 18:21:31 +0900

    주석추가

--- 3_all_about_object/7_this.js
+++ 3_all_about_object/7_this.js
@@ -10,8 +10,8 @@ const testFunction = function(){
     return this;
 }
 
-console.log(testFunction());
-console.log(testFunction() === global);
+console.log(testFunction()); // 일반 함수의 this는 글로벌에 매핑된다.
+console.log(testFunction() === global); // true
 
 const yuJin = {
     name: '안유진',
@@ -42,15 +42,18 @@ Person.prototype.dance = function(){
 
     return dance2();
 }
-console.log(yuJin2.dance());
+console.log(yuJin2.dance()); // undefined이 춤을 춥니다.
+// 객체의 메서드(객체의 가장 상위레벨 함수)에 this는 자동으로 객체로 매핑이 되지만
+// 그 외의 위치에다 함수를 선언하면 함수의 this는 무조건 global object로 매핑된다.
 
 /**
- * this 키워드가 어떤걸 가르키냐는 세가지만 기억하면된다.
+ * this 키워드가 어떤걸 가르키냐는 세가지만 기억하면된다. **
  * 
  * 1) 일반 함수 호출할땐 this가 최상위 객체 (global 또는 window)를 가리킨다.
  * 2) 메서드로 호출할땐 호출된 객체를 가리킨다.
  * 3) new 키워드를 사용해서 객체를 생성했을땐 객체를 가리킨다.
  */
+// 일반적인 OOP는 메소드안에 this는 무조건 객체에 매핑된다. JS와 다름
 
 /**
  * 1) apply()
@@ -73,6 +76,8 @@ console.log(returnName.apply(yuJin3));
 /**
  * 1) call -> 컴마를 기반으로 아규먼트를 순서대로 넘겨주고
  * 2) apply ->  아규먼트를 리스트로 입력해야한다.
+ *
+ * 이 두개는 바로 실행됨
  */
 function multiply(x, y, z){
     return `${this.name} / 결과값 : ${x * y * z}`;
@@ -83,6 +88,8 @@ console.log(multiply.apply(yuJin3, [3, 4, 5]));
 
 /**
  * bind()
+ *
+ * call과 apply와 다르게 바로실행되지 않음
  */
 const laterFunc = multiply.bind(yuJin3, 3, 4, 5);
 console.log(laterFunc);
