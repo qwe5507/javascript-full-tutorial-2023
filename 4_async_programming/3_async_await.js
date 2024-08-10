92d0542cdd0bb9da9d02e1711a43e35609943199
qwe5507 <apvmffkdls@gmail.com>
2024-08-10 20:26:18 +0900

    주석추가

--- 4_async_programming/3_async_await.js
+++ 4_async_programming/3_async_await.js
@@ -4,12 +4,13 @@
 const getPromise = (seconds)=> new Promise((resolve, reject) => {
     setTimeout(()=>{
         resolve('에러');
+        // reject('에러');
     }, seconds * 1000)
 });
 
 async function runner(){
     try{
-        const result1 = await getPromise(1);
+        const result1 = await getPromise(1); // promise만 await가능
         console.log(result1);
         const result2 = await getPromise(2);
         console.log(result2);
@@ -24,3 +25,4 @@ async function runner(){
 }
 
 runner();
+console.log("실행 끝"); // await는 함수를 일시정지 시키지만 메인 스레드를 막고있진 않아서 이 콘솔이 먼저 찍힌다.
